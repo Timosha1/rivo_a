1f2a60f0f11997124df408e2ea042793cb6d806e
Timosha <johndoe@example.comtim.ilya.timofeev@gmail.com>
2024-10-04 12:37:03 +0400

    Optimized should_be methods for asserting the presence of elements

---
+++ pages/locators.py
@@ -0,0 +1,12 @@
+from selenium.webdriver.common.by import By
+
+
+class OverviewPageLocators():
+    # connect wallet button
+    connect_wallet_locator = (By.XPATH, "")
+
+    # connect wallet options
+    create_rivo_wallet_locator = (By.XPATH, "")
+    rivo_locator = (By.XPATH, "")
+    metamask_locator = (By.XPATH, "")
+    walletconnect_locator = (By.XPATH, "")
