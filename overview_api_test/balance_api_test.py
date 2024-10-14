54db9e12a4fd3bda8ca95514c9e8f02120cfe01e
Timosha <johndoe@example.comtim.ilya.timofeev@gmail.com>
2024-10-14 19:48:37 +0400

    smal api test

--- overview_api_test/balance_api_test.py
+++ overview_api_test/balance_api_test.py
@@ -4,6 +4,6 @@ import requests
 
 url = "https://backend.rivo.xyz/balance/0xd278A92a2bED505A67987D2d597Afd2AB160bB3a"
 
-def test_total_balance():
+def test_balance():
     api_response = requests.get(url)
     assert api_response.status_code == 200, f"Expected status code 200 but got {api_response.status_code}"
